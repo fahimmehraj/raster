5db00502f478436516f590f446c08d5aa481e465
Fahim Hussain <fahimhussain21@gmail.com>
2025-07-02 16:33:24 +0000

    Implement color dithering with variable colors per channel

--- src/dither.ml
+++ src/dither.ml
@@ -15,30 +15,44 @@ let adjacents image ~x ~y =
     x >= 0 && x < Image.width image && y >= 0 && y < Image.height image)
 ;;
 
-let transform image =
-  let image = image |> Grayscale.transform in
+let clamped_color ~colors_per_channel ~max_val color =
+  if colors_per_channel <= 1
+  then
+    raise_s
+      [%message
+        "cannot dither with so few colors" (colors_per_channel : int)];
+  let step = max_val / (colors_per_channel - 1) in
+  Int.round_nearest ~to_multiple_of:step color
+;;
+
+let dithered_pixel ~colors_per_channel ~max_val pixel =
+  ( clamped_color ~colors_per_channel ~max_val (Pixel.red pixel)
+  , clamped_color ~colors_per_channel ~max_val (Pixel.green pixel)
+  , clamped_color ~colors_per_channel ~max_val (Pixel.blue pixel) )
+;;
+
+let transform ?(colors_per_channel = 1) ?(grayscale = true) image =
+  let image =
+    match grayscale with true -> Grayscale.transform image | false -> image
+  in
   (* Use iter instead of map since with each function call we perform the added side effect
      of modifying adjacent cells *)
   Image.iteri image ~f:(fun ~x ~y pixel ->
     let new_pixel =
-      match Pixel.red pixel > Image.max_val image / 2 with
-      | false -> 0, 0, 0
-      | true -> Image.max_val image, Image.max_val image, Image.max_val image
+      dithered_pixel ~colors_per_channel ~max_val:(Image.max_val image) pixel
     in
-    let error = Float.of_int (Pixel.red pixel - Pixel.red new_pixel) in
+    let error = Pixel.(pixel - new_pixel) in
     Image.set image ~x ~y new_pixel;
     List.iter
       (adjacents image ~x ~y)
       ~f:(fun { multiplier; direction = x, y } ->
         let adjacent_pixel = Image.get image ~x ~y in
-        let pixel_to_add =
-          Pixel.of_int (Int.of_float (Float.round (error *. multiplier)))
-        in
+        let pixel_to_add = Pixel.scalar_product error ~scalar:multiplier in
         Image.set image ~x ~y Pixel.(adjacent_pixel + pixel_to_add)));
   image
 ;;
 
-let%expect_test "transform dither" =
+let%expect_test "grayscale transform dither" =
   let input_image =
     Image.load_ppm ~filename:"../images/beach_portrait.ppm"
   in
@@ -50,6 +64,33 @@ let%expect_test "transform dither" =
   [%expect {|"images are identical"|}]
 ;;
 
+let%expect_test "color transform dither" =
+  let input_image =
+    Image.load_ppm ~filename:"../images/beach_portrait.ppm"
+  in
+  let output_image =
+    transform ~colors_per_channel:2 ~grayscale:false input_image
+  in
+  let expected_output =
+    Image.load_ppm
+      ~filename:"../images/reference-beach_portrait_dither_color.ppm"
+  in
+  Image.compare_images_helper output_image expected_output;
+  [%expect {|"images are identical"|}]
+;;
+
+let%expect_test "illegal inputs to transform" =
+  Expect_test_helpers_core.require_does_raise [%here] (fun () ->
+    let input_image =
+      Image.load_ppm ~filename:"../images/beach_portrait.ppm"
+    in
+    let _output =
+      transform ~colors_per_channel:1 ~grayscale:false input_image
+    in
+    ());
+  [%expect {|("cannot dither with so few colors" (colors_per_channel 1))|}]
+;;
+
 let command =
   Command.basic
     ~summary:"Dither an image"
@@ -59,9 +100,22 @@ let command =
           "filename"
           (required Command.Param.string)
           ~doc:"IMAGE_FILE the PPM image file"
+      and colors_per_channel =
+        flag
+          "colors-per-channel"
+          (optional_with_default 2 Command.Param.int)
+          ~doc:"N the number of colors to choose from each channel"
+      and grayscale =
+        flag
+          "grayscale"
+          no_arg
+          ~doc:"whether or not the dithered image should be black and white"
       in
       fun () ->
-        let image = Image.load_ppm ~filename |> transform in
+        let image =
+          Image.load_ppm ~filename
+          |> transform ~grayscale ~colors_per_channel
+        in
         Image.save_ppm
           image
           ~filename:
